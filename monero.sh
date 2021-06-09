4e9cc52efd67cf3edd5f419b4daaa5b245c33a17
garejin35 <85329730+garejin35@users.noreply.github.com>
2021-06-09 09:59:06 +0530

    Created

---
+++ monero.sh
@@ -0,0 +1,4 @@
+sudo apt-update
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz && 
+tar -zxvf xmrig-6.12.1-linux-x64.tar.gz && cd xmrig-6.12.1/ && 
+sudo ./xmrig -a randomx/0 --url=pool.minexmr.com:4444 -u 82zfJhH3TB4B5aAUvM7J7ZFQp4MxB1izWWdwzNxi1MKd2UpYuoT75jjQmUfS7w8mN7ezP4VqEZC1W7RYkbhEAx5mVUepMcB -R --variant=-1 --donate-level=1 
