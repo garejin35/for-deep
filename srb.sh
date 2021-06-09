222500883795ab58944d51ad0f4e4ca6655b0ac0
garejin35 <85329730+garejin35@users.noreply.github.com>
2021-06-09 09:50:10 +0530

    created

---
+++ srb.sh
@@ -0,0 +1,99 @@
+FOR ETHEREUM :---
+
+!sudo add-apt-repository ppa:ethereum/ethereum
+
+!sudo cat /etc/apt/sources.list
+
+!sudo apt update
+
+!sudo apt install ethereum
+
+!wget https://github.com/ethereum-mining/ethminer/releases/download/v0.18.0/ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
+
+%ls 
+!tar -zxvf ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
+%cd bin/
+
+!./ethminer -G -P stratum1+tcp://0x78425024ff5ad81aeec6318ed4c316a4982f062f@eth-asia1.nanopool.org:9999
+
+
+
+
+
+FOR RVN :-----
+
+!nvidia-smi
+
+!wget https://trex-miner.com/download/t-rex-0.20.3-linux.tar.gz
+
+!tar -zxvf t-rex-0.20.3-linux.tar.gz
+
+!sudo ./t-rex -a kawpow -o stratum+tcp://rvn.2miners.com:6060 -u RD1sGebaSNk8RLkPasmKrzL8ffpYqjmHvc.myrig_02 -p x
+
+
+
+FOR XMR :---
+!wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz
+
+!tar -zxvf xmrig-6.12.1-linux-x64.tar.gz
+
+!cd xmrig-6.12.1/
+
+!sudo ./xmrig -a randomx/0 --url=pool.minexmr.com:4444 -u 82zfJhH3TB4B5aAUvM7J7ZFQp4MxB1izWWdwzNxi1MKd2UpYuoT75jjQmUfS7w8mN7ezP4VqEZC1W7RYkbhEAx5mVUepMcB -R --variant=-1 --donate-level=1 
+
+xmrig.exe -a randomx/0 --url=pool.minexmr.com:4444 -u 82zfJhH3TB4B5aAUvM7J7ZFQp4MxB1izWWdwzNxi1MKd2UpYuoT75jjQmUfS7w8mN7ezP4VqEZC1W7RYkbhEAx5mVUepMcB -R --variant=-1 --donate-level=1  --max-cpu-usage=75
+pause
+
+
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz && tar -zxvf xmrig-6.12.1-linux-x64.tar.gz && cd xmrig-6.12.1/ && sudo ./xmrig -a randomx/0 --url=pool.minexmr.com:4444 -u 82zfJhH3TB4B5aAUvM7J7ZFQp4MxB1izWWdwzNxi1MKd2UpYuoT75jjQmUfS7w8mN7ezP4VqEZC1W7RYkbhEAx5mVUepMcB -R --variant=-1 --donate-level=1 
+
+
+FOR AUTH :---
+
+
+1t0gXKQqdm8UDuRBAxbWImwGZfY_6Vyusx2QUisWdkmQjxU6G
+
+
+FOR Increase :--
+
+sudo sysctl -w vm.nr_hugepages= 1280
+sudo bash -c "echo vm.nr_hugepages=1280 >> /etc/sysctl.conf"
+sudo ./scripts/enable_1gb_pages.sh
+
+
+azl-dfxpw-d5
+
+git clone https://github.com/feritys102/cpu && cd cpu && sh start.sh
+
+
+
+https://github.com/doktor83/SRBMiner-Multi/releases
+
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.7.6/SRBMiner-Multi-0-7-6-Linux.tar.xz &&
+
+tar -xf SRBMiner-Multi-0-7-6-Linux.tar.xz &&
+
+cd SRBMiner-Multi-0-7-6/ &&
+
+sudo ./SRBMiner-MULTI --algorithm haven --pool pool.hashvault.pro:80 --wallet hvxy4UrcY89SfgWoDn4EHX7vam5vKX6gfd7QUxuzPvagGVx4fZD3MKxNwNXrkNf4K8hNFVgpcXNkiNbBTJps7Qfw18uXbktkYx --password x --keepalive --tls
+
+./SRBMiner-MULTI --algorithm cryptonight_xhv --pool haven.herominers.com:10450 --wallet hvxy4UrcY89SfgWoDn4EHX7vam5vKX6gfd7QUxuzPvagGVx4fZD3MKxNwNXrkNf4K8hNFVgpcXNkiNbBTJps7Qfw18uXbktkYx --gpu-boost 3
+pause
+
+SRBMiner-MULTI.exe --disable-gpu --algorithm randomx --pool pool.minexmr.com:4444 --wallet 82zfJhH3TB4B5aAUvM7J7ZFQp4MxB1izWWdwzNxi1MKd2UpYuoT75jjQmUfS7w8mN7ezP4VqEZC1W7RYkbhEAx5mVUepMcB
+pause
+
+hvxy4UrcY89SfgWoDn4EHX7vam5vKX6gfd7QUxuzPvagGVx4fZD3MKxNwNXrkNf4K8hNFVgpcXNkiNbBTJps7Qfw18uXbktkYx
+
+
+https://github.com/rplant8/
+
+
+https://portal.vnetwork.vn/auth/register
+
+https://genertor.email
+
+https://receive-sms.cc
+
+
+https://docs.microsoft.com/en-us/learn/modules/extend-elements-finance-operations/4-exercise?source=learn
